22b9145f4cbe5af50c2ce225f4c63387a143f813
Narita Haku <bokong8119@163.com>
2025-06-28 19:29:19 +0800

    Add files via upload

---
+++ added/赋分后学生排名（独立）.py
@@ -0,0 +1,32 @@
+#兼容主程序V1.0和V2.0，本程序是计算赋分后的每位同学的排名（分为班级内部和全部）（独立的程序，请务必运行主程序再运行插件）
+import pandas as pd
+flag=False   #False表示未进行正确性检查或者state状态填写有误，True表示state状态填写正确
+state=0 #0表示班级内部排名，1表示全部学生排名
+def state0(df_orignal,class_list):
+    result= pd.DataFrame() 
+    for i in class_list:
+        df_temp=df_orignal[df_orignal['班级']==i]
+        df_temp=df_temp.sort_values('赋分（取高）',ascending=False)
+        result=pd.concat([result, df_temp], ignore_index=True)    #DataFrame合并操作,ignore_index=True表示索引重新排序
+    return result
+def get_class_list(df):
+    list = df['班级'].unique().tolist()   #读取班级信息
+    return list
+def state1(df_orignal):
+    df_orignal=df_orignal.sort_values('赋分（取高）',ascending=False)
+    return df_orignal
+df=pd.read_excel('成绩（结合首考）.xlsx')
+if state==0:
+    list=get_class_list(df)
+    df1=state0(df,list)
+    flag=True
+elif state==1:
+    df1=state1(df)
+    flag=True
+else:
+    print('输入的状态有误，请检查')
+if flag==True:
+    if state==1:
+        df1.to_excel("赋分后学生排名.xlsx", index=False)
+    elif state==0:
+        df1.to_excel("赋分后学生排名(以班级独立排名).xlsx", index=False)
